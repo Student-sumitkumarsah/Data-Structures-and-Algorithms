505c140cf1eb12c2f7e69656f32ccc59272291e1
sumit kumar sah <sumitkumar92365@gmail.com>
2024-12-13 00:05:27 +0530

    Check Or Even Or Odd Number

---
+++ DAY1 Basic/evenorOdd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class evenorOdd {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num%2==0){
+            System.out.println("this number is Even");
+        }
+        else{
+            System.out.println("this number is Odd");
+        }
+        
+    }
+    
+}
