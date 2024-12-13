ef8a5a4313e86dbc745ec25f9c4b89dd1b0917d6
sumit kumar sah <sumitkumar92365@gmail.com>
2024-12-13 21:49:34 +0530

    Add 1 Given All Array Element

---
+++ ARRAY/AddOneAllElement.java
@@ -0,0 +1,18 @@
+public class AddOneAllElement {
+    public static void update(int marks[]){
+        for(int i=0;i<marks.length;i++){
+            marks[i]=marks[i]+1;
+        }
+
+    }
+    public static void main(String[] args) {
+        int marks[]={20,34,43};
+        update(marks);
+
+        for(int i=0;i<marks.length;i++){
+            System.out.print(marks[i]+" ");
+        }
+        
+    }
+    
+}
